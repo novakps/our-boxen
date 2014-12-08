7bb6606fec89b4c48100cb6d89fd38fe7aceea3a
Paul Novak <paul.novak@schrodinger.com>
2014-12-08 15:54:47 -0500

    configure osx

--- modules/people/manifests/novakps.pp
+++ modules/people/manifests/novakps.pp
@@ -1,5 +1,8 @@
 class people::novakps {
   include emacs
   include zsh
-  include puppet-osx
+  include osx::dock::clear_dock
+  include osx::dock::autohide
+  include osx::keyboard::capslock_to_control
+  include osx::dock::position
 }
