eb7672e645d72f928db9c29ecd5780364ee01bc4
Paul Novak <paul.novak@schrodinger.com>
2014-12-09 14:27:38 -0500

    revert for dotfiles

--- modules/people/manifests/novakps.pp
+++ modules/people/manifests/novakps.pp
@@ -8,10 +8,16 @@ class people::novakps {
   include osx::keyboard::capslock_to_control
   include osx::dock::position
 
-  $home     = "/Users/${::boxen_user}"
-  $dotfiles = "${home}/dotfiles"
+  $home = "/Users/${::boxen_user}"
+  $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
 
-  repository { $dotfiles:
-    source  => 'novakps/dotfiles'
+  repository {
+    $dotfiles_dir: source = > "${::github_user}/dotfiles"
+  }
+
+  file {
+    "${home}/.zshrc": ensure = > link,
+    target = > "${dotfiles_dir}/.zshrc",
+    require = > Repository[$dotfiles_dir]
   }
 }
