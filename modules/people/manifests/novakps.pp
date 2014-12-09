fdb53a0d2f0ab8ae91aa8d7ba6b970eb919f6df7
Paul Novak <paul.novak@schrodinger.com>
2014-12-09 15:22:05 -0500

    revert

--- modules/people/manifests/novakps.pp
+++ modules/people/manifests/novakps.pp
@@ -11,19 +11,6 @@ class people::novakps {
   $home = "/Users/${::boxen_user}"
   $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
 
-  # Changes the default shell to the zsh version we get from Homebrew
-  # Uses the osx_chsh type out of boxen/puppet-osx
-  osx_chsh { $::luser:
-    shell   => '/opt/boxen/homebrew/bin/zsh',
-    require => Package['zsh'],
-  }
-
-  file_line { 'add zsh to /etc/shells':
-    path    => '/etc/shells',
-    line    => "${boxen::config::homebrewdir}/bin/zsh",
-    require => Package['zsh'],
-  }
-
   repository {
     $dotfiles_dir: source => "novakps/dotfiles"
   }
