f7551b1e1a696d0f839d430e728ce998a9ccf298
Paul Novak <paul.novak@schrodinger.com>
2014-12-09 09:00:15 -0500

    dotfiles

--- modules/people/manifests/novakps.pp
+++ modules/people/manifests/novakps.pp
@@ -5,4 +5,17 @@ class people::novakps {
   include osx::dock::autohide
   include osx::keyboard::capslock_to_control
   include osx::dock::position
+
+  $home = "/Users/${::boxen_user}"
+  $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
+
+  repository { $dotfiles_dir:
+    source => "${::github_user}/dotfiles"
+  }
+
+  file { "${home}/.zshrc":
+    ensure  => link,
+    target  => "${dotfiles_dir}/.zshrc",
+    require => Repository[$dotfiles_dir]
+  }
 }
