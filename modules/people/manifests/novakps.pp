e4d2cfe3c7272759b6aa67fae2128a04658d919e
Paul Novak <paul.novak@schrodinger.com>
2014-12-09 09:30:48 -0500

    zsh stuff

--- modules/people/manifests/novakps.pp
+++ modules/people/manifests/novakps.pp
@@ -1,6 +1,8 @@
 class people::novakps {
   include emacs
   include zsh
+  include ohmyzsh
+  include zshgitprompt
   include osx::dock::clear_dock
   include osx::dock::autohide
   include osx::keyboard::capslock_to_control
